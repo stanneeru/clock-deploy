d69dfe80724f7dcab4fde04e91dcdf04078b712b
sri26 <thefrequent99@gmail.com>
2019-05-29 12:25:31 -0400

    Updates

---
+++ precache-manifest.cf0c19c8ba574f52cc008d878b2875c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a81c02e2b14f0ce21f475180cf9997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb866d732aad340cd614",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "996e8de7a2507410c5d7",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "eb866d732aad340cd614",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "996e8de7a2507410c5d7",
+    "url": "/static/js/main.addb0924.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
