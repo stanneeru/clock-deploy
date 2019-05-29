c0691d46fde21503b21765d91abad3fed2f50ada
sri26 <thefrequent99@gmail.com>
2019-05-29 12:36:49 -0400

    test code

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Clock extends Component {
   constructor(props){
@@ -53,5 +54,5 @@ return(
 )
   }
 }
-
 export default Clock;
+// test clock
